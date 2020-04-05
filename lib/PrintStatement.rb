731ec8df59397fafebd8ca05bcd4aaca1d03e93f
Arjun Roy <arjunroychoudhry4059@gmail.com>
2020-04-06 00:51:46 +0100

    Print Statement Feature

---
+++ lib/PrintStatement.rb
@@ -0,0 +1,23 @@
+class PrintStatement
+
+attr_accessor :transactions
+
+  def initialize
+    @transactions = []
+  end
+
+  def head
+    @head = "date       || credit || debit || balance\n"
+  end
+
+  def print_statement
+    body_of_statement = ""
+    @transactions.each do |transaction|
+      body_of_statement << "#{transaction.date} || #{transaction.credit}      || #{transaction.debit}     || #{transaction.total_balance}\n"
+    end
+   puts head + body_of_statement
+ end
+
+
+
+end
---
+++ spec/features/printBankStatement_spec.rb
@@ -0,0 +1,15 @@
+describe 'Feature test printstatement' do
+
+    it 'Print_statement method prints the bank statement' do
+
+account = Account.new
+      mock = double('Date mock', date: 'date')
+      allow(account).to receive(:date).and_return(mock)
+      # date = Time.now.strftime("%d/%m/%Y")
+      account.withdraw(9)
+      account.deposit(7)
+      output = "date       || credit || debit || balance\n#{mock} || 0      || 9     || -9\n#{mock} || 7      || 0     || -2\n"
+      expect { account.printstatement.print_statement }.to output(output).to_stdout
+    end
+
+end
